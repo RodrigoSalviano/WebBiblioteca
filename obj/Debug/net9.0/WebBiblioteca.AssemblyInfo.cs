7aba9ebb3d1a730e5e8b6412d98aaf9cac23357d
Rodrigo <rodrigosalviano80@gmail.com>
2025-11-25 20:41:42 -0300

    Feat: Created migrations
    Foi criado as migrations para a criação do banco de dados, por meio do console. Necessario dotnet tool install -- global dotnet - ef , após isso só utilizar o comando dotnet ef database update , assim cria-ra o banco automaticamente sem a nessecidade de utilizar comandos sql.

--- obj/Debug/net9.0/WebBiblioteca.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebBiblioteca.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebBiblioteca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7b645ccf7fad53af3e87fd321ce0673af65ced7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c228a051f7bbc96271de1b9c0007a31bc3a8af9")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebBiblioteca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebBiblioteca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
