7c228a051f7bbc96271de1b9c0007a31bc3a8af9
Rodrigo <rodrigosalviano80@gmail.com>
2025-11-25 19:38:25 -0300

    Feat: Creating classes and connecting to the database.
    Criei as classes Livros, Autores, Editoras e Conetexto. E foi adicionado  o ConnectionString no appsettings

---
+++ Models/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebBiblioteca.Models
+{
+    public class Contexto:DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+        }
+        public DbSet<Livros> Livros { get; set; }
+        public DbSet<Autores> Autores { get; set; }
+        public DbSet<Editora> Editora { get; set; }
+    }
+}
---
+++ Models/Livros.cs
@@ -0,0 +1,17 @@
+﻿namespace WebBiblioteca.Models
+{
+    public class Livros
+    {
+        public int id { get; set; }
+        public string titulo { get; set; }
+        public string genero { get; set; }
+        public int Paginas { get; set; }
+
+        public Livros(int paginas)
+        {
+            Paginas=paginas;
+        }
+
+        public int qtd { get; set; }
+    }
+}
