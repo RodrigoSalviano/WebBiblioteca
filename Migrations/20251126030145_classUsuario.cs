9d1675db5f6e85cbdedb6654b02874a46b81e4b6
Rodrigo <rodrigosalviano80@gmail.com>
2025-11-26 00:07:21 -0300

    Feat: Created new class Usuario

---
+++ Migrations/20251126030145_classUsuario.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebBiblioteca.Migrations
+{
+    /// <inheritdoc />
+    public partial class classUsuario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    senha = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    acesso = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuario");
+        }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿namespace WebBiblioteca.Models
+{
+    public class Usuario
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string email { get; set; }
+        public string senha { get; set; }
+        public string acesso { get; set; }
+
+        public Usuario() { }
+    }
+}
