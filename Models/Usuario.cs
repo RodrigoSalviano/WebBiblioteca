de51fa47f64769b59357a6b090a9b23fc880f7a7
Rodrigo <rodrigosalviano80@gmail.com>
2025-11-26 11:51:15 -0300

    Refeactury: Added annotations in usuario class
    criei uma nova migration, pois alterei a classe usuario. Adicionando annotations para restringir e exigir campos

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,11 +1,24 @@
-﻿namespace WebBiblioteca.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebBiblioteca.Models
 {
+    [Table("Usuarios")]
     public class Usuario
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] 
         public int id { get; set; }
+        [StringLength(100)]
+        [Required]
         public string nome { get; set; }
+        [StringLength(120)]
+        [Required]
         public string email { get; set; }
+        [StringLength(100)]
+        [Required]
         public string senha { get; set; }
+        [Required]
         public string acesso { get; set; }
 
         public Usuario() { }
